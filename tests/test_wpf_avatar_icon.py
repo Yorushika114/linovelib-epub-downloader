19dde27f7ff85c46c3281862c6b27f1ebb9d9384
yanghaoCSXY <2322339217@qq.com>
2026-09-04 00:38:53 +0800

    docs: attribute WPF avatar icon source

--- tests/test_wpf_avatar_icon.py
+++ tests/test_wpf_avatar_icon.py
@@ -6,6 +6,7 @@ def test_wpf_window_uses_local_anime_avatar_icon():
     xaml = (root / "wpf" / "LinovelibDesktop" / "MainWindow.xaml").read_text(encoding="utf-8")
 
     assert 'Icon="Assets/anime-avatar-icons8.jpg"' not in xaml
+    assert 'NavigateUri="https://icons8.com"' in xaml
     assert (root / "wpf" / "LinovelibDesktop" / "Assets" / "anime-avatar-icons8.jpg").is_file()
     assert (root / "wpf" / "LinovelibDesktop" / "Assets" / "anime-avatar-icons8.ico").is_file()
     project = (root / "wpf" / "LinovelibDesktop" / "LinovelibDesktop.csproj").read_text(encoding="utf-8")
