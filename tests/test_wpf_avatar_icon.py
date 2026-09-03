270c5d810c50fbea1280d271d66edb3b2224c98e
yanghaoCSXY <2322339217@qq.com>
2026-09-04 00:31:09 +0800

    fix: package WPF avatar icon resource

--- tests/test_wpf_avatar_icon.py
+++ tests/test_wpf_avatar_icon.py
@@ -7,3 +7,5 @@ def test_wpf_window_uses_local_anime_avatar_icon():
 
     assert 'Icon="Assets/anime-avatar-icons8.jpg"' in xaml
     assert (root / "wpf" / "LinovelibDesktop" / "Assets" / "anime-avatar-icons8.jpg").is_file()
+    project = (root / "wpf" / "LinovelibDesktop" / "LinovelibDesktop.csproj").read_text(encoding="utf-8")
+    assert '<Resource Include="Assets\\anime-avatar-icons8.jpg" />' in project
